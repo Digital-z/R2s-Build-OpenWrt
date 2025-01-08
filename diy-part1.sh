a0ad00484a0442f6be723c315807e6b2e000d69f
Xiaozhang <76113931+Digital-z@users.noreply.github.com>
2025-01-08 16:17:00 +0800

    Update diy-part1.sh
    添加额外软件包

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git lede/package/lean
+git clone https://github.com/SAENE/luci-theme-design.git lede/package/lean
